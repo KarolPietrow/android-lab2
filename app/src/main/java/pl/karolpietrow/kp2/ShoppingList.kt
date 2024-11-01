a4ae60eabb7a7991223bf9dee74c092f0aaf604e
Karol Pietrów <karolpietrow2004@gmail.com>
2024-11-01 15:43:14 +0100

    v4 - Fix horizontal alignment on wide displays

--- app/src/main/java/pl/karolpietrow/kp2/ShoppingList.kt
+++ app/src/main/java/pl/karolpietrow/kp2/ShoppingList.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import java.util.UUID
